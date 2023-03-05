b1cb386a1ce7f923676ed12778428b4ae137392b
August McAllister <august.chm@gmail.com>
2023-03-04 18:07:51 -0600

    feat: added auth files to lib

--- lib/api.ts
+++ lib/api.ts
@@ -1,35 +1,45 @@
-const fetcher = async ({ url, method, body, json = true }) => {
+const fetcher = async ({
+  url,
+  method,
+  body,
+  json = true,
+}: {
+  url: string;
+  method: string;
+  body: object;
+  json?: boolean;
+}) => {
   const res = await fetch(url, {
     method,
     ...(body && { body: JSON.stringify(body) }), // weird syntax, but this is just a conditional
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
 
   if (!res.ok) {
-    throw new Error('API Error')
+    throw new Error("API Error");
   }
 
   if (json) {
     const data = await res.json();
     return data.data;
   }
-}
+};
 
-export const register = async (user) => {
+export const register = async (user: object) => {
   return fetcher({
-    url: 'api/register',
-    method: 'POST',
+    url: "api/register",
+    method: "POST",
     body: user,
-  })
-}
+  });
+};
 
-export const signin = async (user) => {
+export const signin = async (user: object) => {
   return fetcher({
-    url: 'api/signin',
-    method: 'POST',
+    url: "api/signin",
+    method: "POST",
     body: user,
-  })
-}
+  });
+};
